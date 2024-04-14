9e0f01a169bb8a7ece0fb3fcd5c98d5c1608be0d
Mohan Gangadhar Gudey <mohangangadhar@gmail.com>
2024-04-14 17:00:56 +0530

    first commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
   imports: [
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+    }),
     IonicModule,
     CommonModule,
     FormsModule,
